ba56923851ddf41e5bc07da3965581c008ba0a3a
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-10-13 14:13:18 +0800

    Make test pass

--- backend/server.go
+++ backend/server.go
@@ -3,8 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(w, "20")
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	if player == "patat" {
+		fmt.Fprint(w, "20")
+		return
+	}
+	if player == "floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
 }
